30642226c73631351501bd0bcc241035a76a818c
emon-qualyval <emon.das@qualyval.com>
2022-08-27 22:06:30 +0600

    updated social icons

--- src/components/LoginAndRegister/LoginAndRegister.jsx
+++ src/components/LoginAndRegister/LoginAndRegister.jsx
@@ -412,23 +412,6 @@ const LoginAndRegister = () => {
                 className="btn solid newPassword hidden"
                 onClick={changePassword}
               />
-              <p className="loginSocial social-text">
-                Or Sign in with social platforms
-              </p>
-              <div className="loginSocial social-media">
-                <a href=" " className="social-icon">
-                  <i className="fab fa-facebook-f"></i>
-                </a>
-                <a href=" " className="social-icon">
-                  <i className="fab fa-twitter"></i>
-                </a>
-                <a href=" " className="social-icon">
-                  <i className="fab fa-google"></i>
-                </a>
-                <a href=" " className="social-icon">
-                  <i className="fab fa-linkedin-in"></i>
-                </a>
-              </div>
             </form>
             <form action="#" className="sign-up-form">
               <h2 className="title signUpI">Sign up</h2>
@@ -470,21 +453,6 @@ const LoginAndRegister = () => {
                 className="btn otp"
                 value="Confirm Email"
               />
-              <p className="social-text">Or Sign up with social platforms</p>
-              <div className="social-media">
-                <a href=" " className="social-icon">
-                  <i className="fab fa-facebook-f"></i>
-                </a>
-                <a href=" " className="social-icon">
-                  <i className="fab fa-twitter"></i>
-                </a>
-                <a href=" " className="social-icon">
-                  <i className="fab fa-google"></i>
-                </a>
-                <a href=" " className="social-icon">
-                  <i className="fab fa-linkedin-in"></i>
-                </a>
-              </div>
             </form>
           </div>
         </div>
